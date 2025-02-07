fc902d978956d103822692eb79850e1fa55be1c3
Sathya Prakash J <75614237+Sathya-github-del@users.noreply.github.com>
2025-02-07 14:43:37 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173', // Local development
-    'https://mentee-form-theta.vercel.app/' // Replace with your Vercel domain
+    'https://mentee-form-six.vercel.app/' // Replace with your Vercel domain
   ],
   credentials: true
 }));
